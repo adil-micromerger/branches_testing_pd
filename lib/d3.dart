6e82dc0347313fefc0a9e04033e84fd37c23e1ba
adil-micromerger <132429805+adil-micromerger@users.noreply.github.com>
2024-06-03 14:52:20 +0500

    d3 added to f1

---
+++ lib/d3.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class D3 extends StatelessWidget {
+  const D3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("D3"),
+      ),
+      body: const Column(
+        children: [
+          Text("D3 added to f1"),
+        ],
+      ),
+    );
+  }
+}
