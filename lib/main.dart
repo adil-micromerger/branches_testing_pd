810f23f9481ecdcd1d16317b34d7d979e03e79c1
adil-micromerger <132429805+adil-micromerger@users.noreply.github.com>
2024-06-04 17:09:08 +0500

    changes in script

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
